b5920c5864ebea17762346f7ec9a9f7671b02b81
Serhii Zaika <ewentualnie.sz@gmail.com>
2024-09-25 22:35:31 +0300

    add typeorm

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { dataSourceOptionst } from './database/db-config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
+  imports: [TypeOrmModule.forRoot(dataSourceOptionst)],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ src/database/db-config.ts
@@ -0,0 +1,20 @@
+import { DataSource, DataSourceOptions } from 'typeorm';
+
+export const dataSourceOptionst: DataSourceOptions = {
+  type: 'postgres',
+  host: process.env.PGHOST,
+  port: 5432,
+  username: process.env.PGUSER,
+  password: process.env.PGPASSWORD,
+  database: process.env.PGDATABASE,
+  entities: ['dist/**/*entity.js'],
+  migrations: ['dist/database/migrations/*.js'],
+  migrationsRun: true,
+  synchronize: true,
+  logging: ['warn', 'error'],
+  ssl: {
+    rejectUnauthorized: false,
+  },
+};
+
+export const dataSourse = new DataSource(dataSourceOptionst);
