3d4fbb370c1c0200a8710688b7d2575837957744
Irka-Bodnariuk <bodnariuk.iryna1@gmail.com>
2024-10-06 17:45:34 +0300

    feat: install aws sdk and create endpoint GET /public/logo

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,29 @@
 import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import * as AWS from 'aws-sdk';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  private s3: AWS.S3;
+
+  constructor(private configService: ConfigService) {
+    this.s3 = new AWS.S3({
+      accessKeyId: this.configService.get('AWS_ACCESS_KEY_ID'),
+      secretAccessKey: this.configService.get('AWS_SECRET_ACCESS_KEY'),
+      region: this.configService.get('AWS_REGION'),
+    });
+  }
+
+  async getLogo(name: string): Promise<any> {
+    const params = {
+      Bucket: this.configService.get('AWS_S3_BUCKET_NAME'),
+      Key: name,
+    };
+    try {
+      const data = await this.s3.getObject(params).promise();
+      return data.Body;
+    } catch (error) {
+      throw new Error('Error fetching logo from S3');
+    }
   }
 }
